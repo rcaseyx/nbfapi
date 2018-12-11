e2557953d1b44b9bf8cf707db04d7deda8683e11
Ryan Casey <rcaseyx@gmail.com>
2018-12-11 13:03:11 -0500

    server side filtering done

--- dogs.js
+++ dogs.js
@@ -34,4 +34,58 @@ router.get('/:id', jwtAuth, (req, res) => {
     });
 });
 
+router.get('/filtered/:age/:size', jwtAuth, (req, res) => {
+  let age = req.params.age;
+  let size = req.params.size;
+
+  if (age === 'any' && size === 'any') {
+    Dog.find()
+      .then(dogs => {
+        res.json({
+          dogs: dogs.map(dog => dog.serialize())
+        })
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      });
+  }
+  else if (age === 'any') {
+    Dog.find({ size: size })
+      .then(dogs => {
+        res.json({
+          dogs: dogs.map(dog => dog.serialize())
+        })
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error'});
+      });
+  }
+  else if (size === 'any') {
+    Dog.find({ age: age })
+      .then(dogs => {
+        res.json({
+          dogs: dogs.map(dog => dog.serialize())
+        })
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error'});
+      });
+  }
+  else {
+    Dog.find({ age: age, size: size})
+      .then(dogs => {
+        res.json({
+          dogs: dogs.map(dog => dog.serialize())
+        })
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error'});
+      });
+  }
+});
+
 module.exports = router;
